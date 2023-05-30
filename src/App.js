796f47a65a9c757bf24eb6e0982a5b55cd0524fd
EAM26 <emuller2607@gmail.com>
2023-05-30 20:46:55 +0200

    Add single pokemon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+
+    const [pokeData, setPokeData] = useState({})
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff')
+                console.log(response.data)
+                setPokeData(response.data)
+            } catch (e) {
+                console.error(e)
+            }
+
+        }
+
+        fetchData();
+
+    }, [])
+
+
+    return (
+        <div>
+            {Object.keys(pokeData).length !== 0 ?
+                (<>
+                    <h1>{pokeData.name}</h1>
+                    <img src={pokeData.sprites.front_default} alt="test"/>
+                    <ul> Abilities
+                        {pokeData.abilities.map((ability, index)=> {
+                            return <li key={index}>{ability.ability.name}</li>
+                        })}
+                    </ul>
+                    <p>Weight: {pokeData.weight}</p>
+                    <p>Moves: {pokeData.moves.length}</p>
+                </>):
+                (<p></p>)}
+
+        </div>
+    );
 }
 
+
 export default App;
+
